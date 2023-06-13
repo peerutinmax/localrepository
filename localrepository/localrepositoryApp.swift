b2020499d57d7c272b6fc3f8181fc30c2d56bfb1
Max Peerutin <peerutinm@gmail.com>
2023-06-13 10:35:26 +0200

    Initial Commit

---
+++ localrepository/localrepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  localrepositoryApp.swift
+//  localrepository
+//
+//  Created by Max Peerutin on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct localrepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
